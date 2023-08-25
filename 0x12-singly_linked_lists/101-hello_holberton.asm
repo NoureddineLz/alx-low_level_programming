0d190369429f5a5ed72823bfb70239e7467c74a9
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-25 12:56:40 -0700

    modified task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,10 @@
-section .data
-    hello db "Hello, Holberton",0
-    format db "%s\n",0
-
 section .text
-    global main
-
-extern printf
-
+global    main
+          extern    printf
 main:
-    mov   edi, format
-    xor   eax, eax
-    call  printf
-    mov   eax, 0
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
