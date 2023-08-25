05e605a3aaf181912b9fcae3ba2ee3267fb3324d
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-25 06:18:08 -0700

    modified all last file

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,13 +9,12 @@
  */
 size_t list_len(const list_t *h)
 {
- size_t count = 0;
-    
- while (h != NULL)
- {
- count++;
- h = h->next;
- }
-    
- return (count);
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+
+return (count);
 }
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,13 @@
  * free_list - frees a linked list
  * @head: list_t list to be freed
  */
-
 void free_list(list_t *head)
 {
- while (head != NULL)
- {
- list_t *temp = head;
- head = head->next;
-        
- free(temp->str);
- free(temp);
- }
+while (head != NULL)
+{
+list_t *temp = head;
+head = head->next;
+
+free(temp->str);
+free(temp);
 }
