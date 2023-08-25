cad363b3baca010e5321c0ce589a99e93ede14d6
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-25 12:32:24 -0700

    md task4

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,15 @@
+section .data
+    hello db "Hello, Holberton",0
+    format db "%s\n",0
+
+section .text
     global main
-     extern printf
+
+extern printf
 
 main:
-		push rbp
-		mov rdi, format
-		mov rsi, hello
-		call printf
-		pop rbp
-		ret
-format: db `Hello, Holberton\n`,0
+    mov   edi, format
+    xor   eax, eax
+    call  printf
+    mov   eax, 0
+    ret
