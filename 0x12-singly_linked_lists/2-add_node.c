81bf18a9cd5a22814285fb86e4d8503db1ac11a9
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-25 05:38:09 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+    list_t *new_node;
+    
+    if (str == NULL)
+        return NULL;
+
+    new_node = (list_t *)malloc(sizeof(list_t));
+    if (new_node == NULL)
+        return NULL;
+
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return NULL;
+    }
+
+    new_node->len = strlen(str);
+    new_node->next = *head;
+    *head = new_node;
+
+    return new_node;
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a linked list
+ * @head: list_t list to be freed
+ */
+
+void free_list(list_t *head)
+{
+ while (head != NULL)
+ {
+ list_t *temp = head;
+ head = head->next;
+        
+ free(temp->str);
+ free(temp);
+ }
+}
