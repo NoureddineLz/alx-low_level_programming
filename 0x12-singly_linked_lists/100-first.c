d19a6251a21b2f55610d9ce2eb3f5d0cf9305684
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-08-25 13:13:35 -0700

    this is modified file

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 
-void before_main(void) __attribute__((constructor);
+void before_main(void) __attribute__((constructor));
 
-/**
- * before_main - prints a sentence before the main
- * function is executed
- */
 void before_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
