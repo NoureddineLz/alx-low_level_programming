815a808402c3c9a30e4320ef459f9289fd637795
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-09-04 17:53:13 -0700

    modified

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,20 +10,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *buffer;
-ssize_t file_descriptor;
-ssize_t bytes_read;
-ssize_t total_bytes_written;
+	char *buffer;
+	ssize_t file_descriptor;
+	ssize_t bytes_read;
+	ssize_t total_bytes_written;
 
-file_descriptor = open(filename, O_RDONLY);
-if (file_descriptor == -1)
-return (0);
+	file_descriptor = open(filename, O_RDONLY);
+	if (file_descriptor == -1)
+		return (0);
 
-buffer = malloc(sizeof(char) * letters);
-bytes_read = read(file_descriptor, buffer, letters);
-total_bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	buffer = malloc(sizeof(char) * letters);
+	bytes_read = read(file_descriptor, buffer, letters);
+	total_bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
-free(buffer);
-close(file_descriptor);
-return (total_bytes_written);
+	free(buffer);
+	close(file_descriptor);
+	return (total_bytes_written);
 }
