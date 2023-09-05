8a406bbfa4e55c8e19b4559a5fde19c6438fdecf
NoureddineLz <lazri.noureddine.contact@gmail.com>
2023-09-04 18:05:35 -0700

    file 2 modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,32 +2,32 @@
 
 /**
  * create_file - Creates a file.
- * @new_filename: A pointer to the name of the file to create.
- * @new_text_content: A pointer to a string to write to the file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_file(const char *new_filename, char *new_text_content)
+int create_file(const char *filename, char *text_content)
 {
-	int new_fd, w, len = 0;
+	int fd, w, len = 0;
 
-	if (new_filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	if (new_text_content != NULL)
+	if (text_content != NULL)
 	{
-		for (len = 0; new_text_content[len];)
+		for (len = 0; text_content[len];)
 			len++;
 	}
 
-	new_fd = open(new_filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(new_fd, new_text_content, len);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
 
-	if (new_fd == -1 || w == -1)
+	if (fd == -1 || w == -1)
 		return (-1);
 
-	close(new_fd);
+	close(fd);
 
 	return (1);
 }
